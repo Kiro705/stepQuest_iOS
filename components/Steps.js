40ac3f14bd87844a6ca5fa26adf806ac3fc5639d
Jackson Sui <kiro705@gmail.com>
2017-11-28 18:39:02 -0500

    got STEPS!

--- components/Steps.js
+++ components/Steps.js
@@ -1,8 +1,47 @@
 import React from 'react'
 import { StyleSheet, Text, View, Image } from 'react-native'
+import AppleHealthKit from 'rn-apple-healthkit'
 
-let stepCountToday = 12345
+let stepCountToday = 'boo'
 
+
+let options = {
+    permissions: {
+        read: ["StepCount"],
+    }
+}
+
+// AppleHealthKit.isAvailable((err, available) => { 
+// 	if (err) { 
+// 		console.log('error initializing Healthkit: ', err)
+// 	} else {
+// 		stepCountToday = 11111
+// 	} // Healthkit is available })
+// })
+AppleHealthKit.initHealthKit(options, (err, results) => {
+    if (err) {
+   		stepCountToday = err.message
+        console.log("error initializing Healthkit: ", err);
+        return;
+    }
+
+    stepCountToday = 'I got here.'
+})
+
+let d = new Date(2017,10,1);
+let optionsSteps = {
+    date: d.toISOString()
+};
+AppleHealthKit.getStepCount(optionsSteps, (err, results) => {
+    if (err) {
+        return;
+    }
+    console.error(results)
+});
+// {
+//     value: '1986-09-01T00:00:00.000-0400',
+//     age: 29
+// }
 
 const styles = StyleSheet.create({
 	container: {
